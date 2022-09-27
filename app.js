13ef56b6436ea8beb1443bd8b45a42b289871e70
dnlbnr <grillVest@web.de>
2022-09-27 11:23:37 +0200

    fix linting issues

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+require('dotenv').config()
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5050;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 5050
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  // eslint-disable-next-line no-console
+  console.log(`server started on port ${PORT}`)
+})
